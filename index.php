89f69fbb50137d1a5021540b3192b666770e82a8
J. Marty Mauthe <jmmauthe@gmail.com>
2026-08-10 23:54:15 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title><?php bloginfo('name'); ?></title>
+  <?php wp_head(); ?>
+</head>
+<body>
+  <h1>Hello from my custom theme</h1>
+  <?php wp_footer(); ?>
+</body>
+</html>
